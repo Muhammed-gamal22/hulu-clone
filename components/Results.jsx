07412c22f76e2048161b5a920363604003018ce1
mohamed gamal 22 <52002657+Muhammed-gamal22@users.noreply.github.com>
2021-10-24 23:10:13 +0200

    Update Results.jsx

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import FlipMove from "react-flip-move";
-import Thumbnail from "./Thumbnail";
+import Thumbnail from "./Thumbnail.jsx";
 const Results = ({ results }) => {
   return (
     <FlipMove className="my-10 px-5 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
